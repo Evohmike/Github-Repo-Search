8125f4b2e69d4bec965448129d7cd9b5f1ea0f02
Evohmike <evohmike@gmail.com>
2018-08-17 12:24:23 +0300

    added class user

---
+++ src/app/users.ts
@@ -0,0 +1,4 @@
+export class Users {
+  // constructor(public Name:string,public html_url:any,public avatar_url,public public_repos:any,public repo_url:any,public bio:any,public created_at:Date){
+
+}
