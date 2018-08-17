ce3cfffe3bc87ecef6f76a2ea85714aed7ae3185
Evohmike <evohmike@gmail.com>
2018-08-17 12:16:26 +0300

    This modifies app component ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routes:Routes=[
+  {path:},
+  {path:}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
